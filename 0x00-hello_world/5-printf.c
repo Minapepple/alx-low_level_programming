fcf9b49a3afb19e80b5e252c8ae12502721a6005
Minapepple <peppleaimina@gmail.com>
2023-08-10 14:25:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - A program that print a line using the printf function
+ *
  * Return: 0 (Success)
  */
 
@@ -9,4 +11,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
-
